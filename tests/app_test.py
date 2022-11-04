b8a2723e038de3e7d5b9bab6df3e09e1d778cf1f
Mina Assaad <assaadm2000@gmail.com>
2022-11-04 11:43:21 -0400

    Added Search Page feature.

--- tests/app_test.py
+++ tests/app_test.py
@@ -81,3 +81,9 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+
+def test_search(client):
+    """Ensure that user can search messages"""
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    return client.get('/search/')
